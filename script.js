20e8245b3fbeb1c255a11943dd1000cc0fd200e6
cheifaaaaaa <cheifaikram@gmail.com>
2023-05-19 15:58:19 +0100

    commt

--- script.js
+++ script.js
@@ -7,11 +7,30 @@ if (menubtn){
 
 
 var icon = document.getElementById("icon");
+
+let localData = localStorage.getItem("theme");
+
+    if (localStorage.getItem("theme") == null) {
+        localStorage.setItem("theme", "light");
+    }
+
+    if(localData == "light"){
+        icon.src = "images/moon.png";
+        document.body.classList.remove("dark-theme");
+    } else if (localData == "dark"){
+        icon.src = "images/sun.png";
+        document.body.classList.add("dark-theme");
+    }
+
+
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
+
     if (document.body.classList.contains("dark-theme")){
         icon.src = "images/sun.png";
+        localStorage.setItem("theme", "dark");
     }else{
         icon.src = "images/moon.png";
+        localStorage.setItem("theme", "light");
     }
 }

62b6e4d26a3a85f3e18f9f3869a96760f463441b
cheifaaaaaa <cheifaikram@gmail.com>
2023-05-19 15:21:50 +0100

    commit

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ var navlinks = document.querySelector('.nav-links');
 if (menubtn){
     menubtn.addEventListener('click',()=>{ navlinks.classList.toggle("mobile-menu") });
 }
+
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png";
+    }else{
+        icon.src = "images/moon.png";
+    }
+}
